7e936ef9e67df4e461988835fcbb18bfd5e1e95d
Shivam <Shivam.Chandarana@softwire.com>
2020-10-23 14:50:02 +0100

    Review changes made

--- SQLDB/Queries/insertData.sql
+++ SQLDB/Queries/insertData.sql
@@ -1,9 +1,11 @@
-INSERT INTO dbo.Users(username, userPw)
+SET IDENTITY_INSERT dbo.Users ON
+INSERT INTO dbo.Users(userId, username)
 VALUES
-	('Shiv', 'ShivPW'),
-	('Benji', 'BenjiPW'),
-	('Frank', 'FrankPW'),
-	('Frida', 'FridaPW');
+	(1, 'Shiv'),
+	(2, 'Benji'),
+	(3, 'Frank'),
+	(4, 'Frida');
+SET IDENTITY_INSERT dbo.Users OFF
 
 INSERT INTO dbo.Books(isbn, title, authors)
 VALUES
@@ -12,14 +14,16 @@ VALUES
 	('978-0747532743', 'Harry Potter and the Philosopher''s Stone', 'J. K. Rowling'),
 	('978-0141346809', 'Percy Jackson and the Lightning Thief', 'Rick Riordan');
 
-INSERT INTO dbo.LibraryBooks(isbn)
+SET IDENTITY_INSERT dbo.LibraryBooks ON
+INSERT INTO dbo.LibraryBooks(bookId, isbn)
 VALUES
-	('978-1407104027'),
-	('978-1407104027'),
-	('978-1407104027'),
-	('978-1406360202'),
-	('978-0747532743'),
-	('978-0141346809');
+	(1, '978-1407104027'),
+	(2, '978-1407104027'),
+	(3, '978-1407104027'),
+	(4, '978-1406360202'),
+	(5, '978-0747532743'),
+	(6, '978-0141346809');
+SET IDENTITY_INSERT dbo.LibraryBooks OFF
 
 INSERT INTO dbo.Loans(userId, bookId, dueDate)
 VALUES
